0bef15df3bf7f79e261b6a33d90c8597e0277b66
harsh170403 <harshdhattarwal17@gmail.com>
2024-11-24 00:05:02 +0530

    commit

--- webpage/templates/src/user_dashboard/User_home.jsx
+++ webpage/templates/src/user_dashboard/User_home.jsx
@@ -1,25 +1,12 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Autoplay } from "swiper/modules";
 import { useRef, useState } from "react";
+import HomePageCard from '../components/HomePageCard';
+
 
 import "swiper/css";
 import "swiper/css/navigation";
 
-const HomePageCard = ({ title, img, link }) => {
-  return (
-    <div className="h-[420px] bg-white z-30 m-3">
-      <div className="text-lg xl:text-xl font-semibold ml-4 mt-4">{title}</div>
-      <div className="h-[300px] m-4">
-        <img
-          className="h-[100%] w-[100%] object-cover"
-          src={img}
-          alt="Home card"
-        />
-      </div>
-      <div className="text-xs xl:text-sm text-blue-400 ml-4">{link}</div>
-    </div>
-  );
-};
 
 const Carousel = () => {
   const [gradientColor, setGradientColor] = useState("rgb(0, 0, 0)");
@@ -82,7 +69,7 @@ const Carousel = () => {
       </Swiper>
 
       <div
-        className="h-[70%]"
+        className="h-[100%]"
         style={{
           background: `linear-gradient(to bottom, ${gradientColor}, rgba(255, 255, 255, 0.6))`,
         }}
